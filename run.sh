bfabcb73dcb7f9942035b133ca5c6ef0cb966a95
Shiv <107373873+SecShiv@users.noreply.github.com>
2025-08-29 15:04:11 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python3 h1androidapp.py
 
-echo "# h1domains\nhackerone \"in-scope\" domains\n\n\`python3 h1androidapp.py\`" > README.md
+echo "# h1android\nhackerone \"in-scope\" apps\n\n\`python3 h1androidapp.py\`" > README.md
 echo "## Android Apps with Bounties (Last Updated `date`)" >> README.md
 echo "\`\`\`" >> README.md
 echo "`cat android_apps_with_bounties.txt`" >> README.md

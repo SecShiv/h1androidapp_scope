3078767bd8d0d993f8dd09c63c6a0fc450678176
Shiv <107373873+SecShiv@users.noreply.github.com>
2025-10-04 16:04:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ echo "## Android Apps with Bounties (Last Updated `date`)" >> README.md
 echo "\`\`\`" >> README.md
 echo "`cat android_apps_with_bounties.txt`" >> README.md
 echo "\`\`\`" >> README.md
+
+pip3 install waymore --break
+waymore -i hackerone.com -mode U -n | grep -oP 'https?://\S+/embedded_submissions/new\S*' | sort -u > hackerone_private.txt
